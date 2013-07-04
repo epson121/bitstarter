448434a331a9595b341609ea60951e58e20302d1
epson121 <traxdater12@gmail.com>
2013-07-04 17:14:58 +0200

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var f = fs.readFileSync('index.html');
+  response.send(f);
 });
 
 var port = process.env.PORT || 5000;
