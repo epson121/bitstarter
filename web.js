73bd5401087465a46b4e77184ec74cf1d3db7b70
epson121 <traxdater12@gmail.com>
2013-07-18 14:31:31 +0200

    minor fix in web,js

--- web.js
+++ web.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var f = fs.readFileSync('ind.html');
+  var f = fs.readFileSync('index.html');
   response.send(f.toString("utf-8", 0, f.length));
 });
 
 var port = process.env.PORT || 5000;
+app.use("/public",express.static(__dirname+"/public"));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
